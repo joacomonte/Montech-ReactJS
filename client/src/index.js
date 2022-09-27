77774d21255c3d4d8079a8904d0c6a8dd3e7515f
Joaquin Montesano <montesano1994@gmail.com>
2022-09-27 10:30:09 -0300

    Revert "update scroll to jump page"
    This reverts commit fdeb21ae37c097990b93802ced51404a91f26d77.

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-    <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
+    <BrowserRouter>
         <App tab="home" />  
     </BrowserRouter>
 );
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 
 const express = require('express') //utilizo libreria express
-const cors = require('cors'); 
-require("dotenv").config();
+const cors = require('cors');
 
 const app = express(); // app an instance of express
 // const mysql = require("mysql2/promise") // npm mysql2 is a module for (async connection??)
@@ -9,7 +8,7 @@ const app = express(); // app an instance of express
 app.use(express.json()); // esta linea y la de abajo corrije bugs por correr localHost
 app.use(cors());
 
-const db = require("./src/models");
+const db = require("./models");
 
 
 
@@ -22,26 +21,23 @@ const db = require("./src/models");
 
 
 // --------MIDDELWARE FOR POSTS
-const postsRouter = require('./src/routes/PostsRoute')  // importing the module.exports=router
+const postsRouter = require('./routes/PostsRoute')  // importing the module.exports=router
 app.use("/posts", postsRouter); // middlewarre for posts
 
 // --------MIDDELWARE FOR COMMENTS
-const commentsRouter = require('./src/routes/CommentsRoute'); // importing the module.exports=router
+const commentsRouter = require('./routes/CommentsRoute'); // importing the module.exports=router
 app.use("/comments", commentsRouter); // middlewarre for Comments
 
 // --------MIDDELWARE FOR USERS
-const usersRouter = require('./src/routes/UsersRoute'); // importing the module.exports=router
+const usersRouter = require('./routes/UsersRoute'); // importing the module.exports=router
 app.use("/auth", usersRouter); // middlewarre for USERS
 
 // --------MIDDELWARE FOR LIKES
-const likesRouter = require('./src/routes/LikesRoute'); // importing the module.exports=router
+const likesRouter = require('./routes/LikesRoute'); // importing the module.exports=router
 app.use("/likes", likesRouter); // middlewarre for LIKES
 
-db.sequelize
-    .sync()
-    .then(() =>{
-        app.listen(process.env.PORT || 3001, () => {
-        console.log("---------ta todo piola por ahora")
-    });
-})
-.catch((err)=>{console.log(err)});
+db.sequelize.sync().then(() =>{
+    app.listen(3001, () => {
+        console.log("----------------------ta todo piola por ahora")
+    })
+});
